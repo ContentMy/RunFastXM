78bcd8d85e4f374ebf03d88aafc16c20ab14d6f2
ContentMy <ContentMy@163.com>
2024-05-15 11:53:36 +0800

    bug：解决因Flow以及协程没有手动取消的情况下，插入数据导致数据被观察更新又反复观察，从而导致展示列表一直刷新，无法响应其他事件的bug

--- module-remind/src/main/java/com/existmg/module_remind/works/RemindInitStatusWork.kt
+++ module-remind/src/main/java/com/existmg/module_remind/works/RemindInitStatusWork.kt
@@ -9,6 +9,8 @@ import com.existmg.library_data.accessor.RemindModuleRoomAccessor
 import com.existmg.library_data.db.entity.RemindEntity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.job
 import kotlinx.coroutines.launch
 import java.util.concurrent.TimeUnit
 
@@ -29,8 +31,10 @@ class RemindInitStatusWork(
 
     private fun updateRemind() {
         val repository = RemindModuleRoomAccessor.getRemindRepository()
+        //TODO：bug 协程里的getAllInProgressReminds在数据库插入数据后，反复执行collect回调，导致数据库数据出现波动从而影响了列表的展示和交互。已解决：后续统一优化时记录并删除
         CoroutineScope(Dispatchers.IO).launch {
             repository.getAllInProgressReminds().collect{ list ->
+                println("初始化启动的work在搞事情")
                 list.forEach {
                     val endTime = it.remindEndTime
                     val isCompleted = if (endTime == 0L){//这里是防止旧数据和异常数据，如果有此类数据，在处理时被标记为true
@@ -53,6 +57,7 @@ class RemindInitStatusWork(
                     )
                     repository.updateRemind(remindEntity)
                 }
+                coroutineContext.job.cancel()//遍历完成，取消协程任务以避免数据库使用的问题
             }
         }
     }
