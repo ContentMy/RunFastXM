fd65baa4861f58d991271cb4807eae2b5e5d481f
ContentMy <ContentMy@163.com>
2024-06-03 00:53:58 +0800

    需求：增加个人页面的隐私页面跳转，内容暂无

--- module-user/src/main/java/com/existmg/module_user/ui/UserFragment.kt
+++ module-user/src/main/java/com/existmg/module_user/ui/UserFragment.kt
@@ -1,12 +1,7 @@
 package com.existmg.module_user.ui
 
 import android.content.Intent
-import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.existmg.library_base.fragment.BaseMvvmFragment
@@ -16,6 +11,7 @@ import com.existmg.library_common.utils.ToastUtil
 import com.existmg.module_user.R
 import com.existmg.module_user.databinding.UserLayoutFragmentBinding
 import com.existmg.module_user.ui.activity.UserAboutAppActivity
+import com.existmg.module_user.ui.activity.UserPrivacyActivity
 import com.existmg.module_user.viewmodel.UserViewModel
 
 /**
@@ -65,7 +61,8 @@ class UserFragment:BaseMvvmFragment<UserViewModel,UserLayoutFragmentBinding>(),
                 startActivity(Intent(requireContext(),UserAboutAppActivity::class.java))
             }
             mBinding.userLlPrivacy->{
-                ToastUtil.showShort(requireContext(), "下版本完成")
+                //跳转到隐私页面
+                startActivity(Intent(requireContext(),UserPrivacyActivity::class.java))
             }
             mBinding.userLlInstructions->{
                 ToastUtil.showShort(requireContext(), "下版本完成")
---
+++ module-user/src/main/java/com/existmg/module_user/ui/activity/UserPrivacyActivity.kt
@@ -0,0 +1,37 @@
+package com.existmg.module_user.ui.activity
+
+import androidx.lifecycle.ViewModelProvider
+import com.existmg.library_base.activity.BaseMVVMActivity
+import com.existmg.library_base.manager.viewModelFactory
+import com.existmg.module_user.R
+import com.existmg.module_user.databinding.UserLayoutPrivacyActivityBinding
+import com.existmg.module_user.viewmodel.UserPrivacyViewModel
+
+/**
+ * @Author ContentMy
+ * @Date 2024/6/3 12:36 AM
+ * @Description
+ */
+class UserPrivacyActivity :BaseMVVMActivity<UserPrivacyViewModel,UserLayoutPrivacyActivityBinding>() {
+    override fun getViewModelClass(): Class<UserPrivacyViewModel> {
+        return UserPrivacyViewModel::class.java
+    }
+
+    override fun bindViewModel() {
+        mBinding.viewModel = mViewModel
+    }
+
+    override fun provideViewModelFactory(): ViewModelProvider.Factory {
+        return viewModelFactory {
+            UserPrivacyViewModel()
+        }
+    }
+
+    override fun getLayoutId(): Int {
+        return R.layout.user_layout_privacy_activity
+    }
+
+    override fun initData() {
+        mBinding.userPrivacyToolbar.uiToolbarTvTitle.text = resources.getText(R.string.user_setting_string_privacy)
+    }
+}
